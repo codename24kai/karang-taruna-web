cf72457c446a55c43d2a7b31c4243fd5c63b5b3b
codename24kai <kaisstevalvaro123@gmail.com>
2025-11-23 13:57:09 +0700

    ubah ke mode awal

--- public/index.php
+++ public/index.php
@@ -1,17 +1,20 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Cek Mode Maintenance
+// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register Auto Loader
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Run The Application (Style Laravel 11)
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
